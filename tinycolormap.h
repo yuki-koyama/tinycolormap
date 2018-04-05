7ce5c96db018af9c66c0d75b768f4d48b3bdf6f2
Yuki Koyama <yuki@koyama.xyz>
2018-04-05 21:22:19 +0900

    Add support of Jet and Hot color schemes

--- tinycolormap.h
+++ tinycolormap.h
@@ -36,12 +36,14 @@ namespace tinycolormap
     
     enum class ColorMapType
     {
-        Heat
+        Heat, Jet, Hot
     };
     
     inline Eigen::Vector3d GetColor(double x, ColorMapType type = ColorMapType::Heat);
     inline Eigen::Vector3d GetHeatColor(double x);
-    
+    inline Eigen::Vector3d GetJetColor(double x);
+    inline Eigen::Vector3d GetHotColor(double x);
+
     //////////////////////////////////////////////////////////////////////////////////
     // Implementation
     //////////////////////////////////////////////////////////////////////////////////
@@ -52,6 +54,10 @@ namespace tinycolormap
         {
             case ColorMapType::Heat:
                 return GetHeatColor(x);
+            case ColorMapType::Jet:
+                return GetJetColor(x);
+            case ColorMapType::Hot:
+                return GetHotColor(x);
             default:
                 break;
         }
@@ -61,11 +67,11 @@ namespace tinycolormap
     {
         x = std::max(0.0, std::min(1.0, x));
         
-        static const Eigen::Vector3d r(1.0, 0.0, 0.0);
-        static const Eigen::Vector3d y(1.0, 1.0, 0.0);
-        static const Eigen::Vector3d g(0.0, 1.0, 0.0);
-        static const Eigen::Vector3d c(0.0, 1.0, 1.0);
-        static const Eigen::Vector3d b(0.0, 0.0, 1.0);
+        const Eigen::Vector3d r(1.0, 0.0, 0.0);
+        const Eigen::Vector3d y(1.0, 1.0, 0.0);
+        const Eigen::Vector3d g(0.0, 1.0, 0.0);
+        const Eigen::Vector3d c(0.0, 1.0, 1.0);
+        const Eigen::Vector3d b(0.0, 0.0, 1.0);
         
         if (x < 0.25)
         {
@@ -86,6 +92,76 @@ namespace tinycolormap
         {
             const double t = (x - 0.75) * 4.0;
             return t * r + (1.0 - t) * y;
+        }
+    }
+    
+    inline Eigen::Vector3d GetJetColor(double x)
+    {
+        x = std::max(0.0, std::min(1.0, x));
+        
+        double r, g, b;
+        
+        constexpr double r_max = 0.8;
+        constexpr double g_max = 1.0;
+        constexpr double b_max = 1.0;
+        
+        constexpr double t = 0.125;
+        
+        if (x < t)
+        {
+            r = 0;
+            g = 0;
+            b = b_max * (0.5 + x / t * 0.5);
+        }
+        else if (x < 3.0 * t)
+        {
+            r = 0;
+            g = g_max * (x - t) / (3.0 * t - t);
+            b = b_max;
+        }
+        else if (x < 5.0 * t)
+        {
+            r = r_max * (x - 3.0 * t) / (5.0 * t - 3.0 * t);
+            g = g_max;
+            b = (b_max - (x - 3.0 * t) / (5.0 * t - 3.0 * t));
+        }
+        else if (x < 7.0 * t)
+        {
+            r = r_max;
+            g = (g_max - (x - 5.0 * t) / (7.0 * t - 5.0 * t));
+            b = 0;
+        }
+        else
+        {
+            r = (r_max - (x - 7.0 * t) / (1.0 - 7.0 * t) * 0.5);
+            g = 0;
+            b = 0;
+        }
+        return Eigen::Vector3d(r, g, b);
+    }
+    
+    inline Eigen::Vector3d GetHotColor(double x)
+    {
+        x = std::max(0.0, std::min(1.0, x));
+        
+        const Eigen::Vector3d r(1.0, 0.0, 0.0);
+        const Eigen::Vector3d g(0.0, 1.0, 0.0);
+        const Eigen::Vector3d b(0.0, 0.0, 1.0);
+
+        if (x < 0.4)
+        {
+            const double t = x / 0.4;
+            return t * r;
+        }
+        else if (x < 0.8)
+        {
+            const double t = (x - 0.4) / (0.8 - 0.4);
+            return r + t * g;
+        }
+        else
+        {
+            const double t = (x - 0.8) * (1.0 - 0.8);
+            return r + g + t * b;
         }
     }
 }
