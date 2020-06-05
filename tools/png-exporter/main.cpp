ade37e4d718373c58b0a44e13583b683794178d9
Shot511 <tomaszgalaj@gmail.com>
2020-06-05 16:35:09 +0200

    Added stb_image_write support which resolves #15

--- tools/png-exporter/main.cpp
+++ tools/png-exporter/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-#include <QImage>
+#include <vector>
 #include <tinycolormap.hpp>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STBI_MSC_SECURE_CRT
+#include "stb_image_write.h"
 
 int main(int argc, char* argv[])
 {
@@ -32,20 +36,24 @@ int main(int argc, char* argv[])
 
     for (const auto& colormap_type : colormap_types)
     {
-        QImage image(width, height, QImage::Format_ARGB32);
-
-        for (int x = 0; x < width; ++ x)
+        std::vector<uint8_t> image_data;
+        image_data.resize(width * height * 3);
+        
+        for (int x = 0; x < width; ++x)
         {
             const double value = static_cast<double>(x) / static_cast<double>(width - 1);
-            const QColor color = tinycolormap::GetColor(value, colormap_type.first).ConvertToQColor();
+            const auto color = tinycolormap::GetColor(value, colormap_type.first);
 
             for (int y = 0; y < height; ++ y)
             {
-                image.setPixel(x, y, color.rgb());
+                int index = 3 * (y * width + x);
+                image_data[index + 0] = uint8_t(color.r() * 255.0);
+                image_data[index + 1] = uint8_t(color.g() * 255.0);
+                image_data[index + 2] = uint8_t(color.b() * 255.0);
             }
         }
 
-        image.save(QString::fromStdString(save_directory_path + "/" + colormap_type.second + ".png"));
+        stbi_write_png((save_directory_path + "/" + colormap_type.second + ".png").c_str(), width, height, 3 /* num_components */, image_data.data(), 0);
     }
 
     return 0;
